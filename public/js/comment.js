381af670e75d41ad6b900ca17821069c7a72cd0c
Nate <n8blake@mac.com>
2021-05-27 18:22:30 -0700

    move showHide comments to its own js file

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,13 +1,9 @@
-// show hide comments on main page
-const showHideComments = (articleId) => {
-	const articleComments = document.querySelector(`#article-${articleId}-comments`);
-	if(articleComments.style.display == 'none'){
-		articleComments.style.display = 'block';
-	} else {
-		articleComments.style.display = 'none';
-	}
-} 
+
 
 // post comment
 
 // delete comment 
+
+
+// add listeners
+document.querySelector('#comment-form').addEventListener('submit', newCommentFormHandler);
---
+++ public/js/commmentShowHide.js
@@ -0,0 +1,9 @@
+// show hide comments on main page
+const showHideComments = (articleId) => {
+	const articleComments = document.querySelector(`#article-${articleId}-comments`);
+	if(articleComments.style.display == 'none'){
+		articleComments.style.display = 'block';
+	} else {
+		articleComments.style.display = 'none';
+	}
+} 
